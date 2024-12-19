8a4552499275a7ac173acd7627e80bdca7a6ea13
lemontea <84667067+icelemontea-2333@users.noreply.github.com>
2024-12-19 20:58:25 +0800

    update

--- src/renderer/src/views/Item/index.vue
+++ src/renderer/src/views/Item/index.vue
@@ -3,12 +3,12 @@
         <section class="item-panel u-s-t-n">
             <div class="item-item">
                 <div class="item-name">经典</div>
-                <div v-if="globalStore.global.type == 0" class="item-button">已装载</div>
+                <div v-if="globalStore.global.type == 0" class="item-button item-button-used">已装载</div>
                 <div v-else class="item-button" @click="onclickEquip(0)">装载</div>
             </div>
             <div v-if="globalStore.global.item.taiko" class="item-item">
                 <div class="item-name">太鼓达人</div>
-                <div v-if="globalStore.global.type == 1" class="item-button">已装载</div>
+                <div v-if="globalStore.global.type == 1" class="item-button item-button-used">已装载</div>
                 <div v-else class="item-button" @click="onclickEquip(1)">装载</div>
             </div>
         </section>
@@ -32,35 +32,48 @@
         font-family: 'Uranus_Pixel';
         .item-panel{
             margin-top: 35px;
-            border-top: rgba(255,255,255,.5) 1px solid;
+            border-top: rgba(255,255,255,.25) 1px dashed;
             height: calc(100vh - 35px);
             box-sizing: border-box;
             padding: 5px 10px;
             .item-item{
-                margin-top: 10px;
-                background: rgba(0, 0, 0, 0.25);
+                margin-top: 5px;
                 color: rgb(255, 255, 255);
-                padding: 5px 5px;
+                background: rgba(0,0,0,.5);
+                padding: 5px;
                 display: flex;
+                flex-direction: column;
+                align-items: center;
+                border: rgba(255,255,255,.5) 1px solid;
+                border-radius: 10px;
                 .item-name{
-                    padding: 0 5px;
-                    border-bottom: rgba(255,255,255,1) 1px solid;
+                    padding: 2px 0 0 0;
                 }
                 .item-button{
-                    position: absolute;
-                    right: 15px;
-                    border: rgba(255,255,255,1) 1px solid;
-                    height: 20px;
-                    width: 50px;
+                    margin-top: 5px;
+                    height: 15px;
+                    width: 150px;
                     font-size: 12px;
+                    border: rgba(255,255,255,1) 1px solid;
+                    background: rgba(0,0,0,.25);
                     border-radius: 15px;
                     cursor: pointer;
                     display: flex;
                     justify-content: center;
                     align-items: center;
+                    transition: rotate .35s;
                     &:hover{
                         background: rgba(255,255,255,1);
                         color: rgba(0,0,0,1);
+                        rotate: 2deg;
+                    }
+                }
+                .item-button-used{
+                    background: rgba(0,0,0,.25);
+                    &:hover{
+                        color: rgb(255, 255, 255);
+                        background: rgba(0,0,0,.25);
+                        rotate: -2deg;
                     }
                 }
             }
