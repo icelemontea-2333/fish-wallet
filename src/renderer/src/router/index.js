e88698e774e70c8cba407814829f7b4fead48397
lemontea <84667067+icelemontea-2333@users.noreply.github.com>
2025-02-06 14:25:51 +0800

    update

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -1,5 +1,10 @@
 //配置路由的地方
 import { createRouter,createWebHashHistory } from 'vue-router'
+import Index from '@renderer/views/Index/index.vue'
+import Record from '@renderer/views/Record/index.vue'
+import Item from '@renderer/views/Item/index.vue'
+import Shop from '@renderer/views/Shop/index.vue'
+import System from '@renderer/views/System/index.vue'
 
 const showMenu = {
 
@@ -15,32 +20,32 @@ const routes = [
     {
         name:'index',
         path:"/index",
-        component: ()=> import('@renderer/views/Index/index.vue'),
+        component: Index,
         meta:{...showMenu,...common,title:'敲敲喵'},
         alias:"/"
     },
     {
         name:'record',
         path:"/record",
-        component: ()=> import('@renderer/views/Record/index.vue'),
+        component: Record,
         meta:{...showMenu,...common,title:'记录喵'},
     },
     {
         name:'item',
         path:"/item",
-        component: ()=> import('@renderer/views/Item/index.vue'),
+        component: Item,
         meta:{...showMenu,...common,title:'包裹喵'},
     },
     {
         name:'shop',
         path:"/shop",
-        component: ()=> import('@renderer/views/Shop/index.vue'),
+        component: Shop,
         meta:{...showMenu,...common,title:'小卖部喵'},
     },
     {
         name:'system',
         path:"/system",
-        component: ()=> import('@renderer/views/System/index.vue'),
+        component: System,
         meta:{...showMenu,...common,isCommand:true,title:'系统设置喵'},
     }
 ]
